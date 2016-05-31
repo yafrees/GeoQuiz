3299922b764ea008fe7761ce0a0dd5e986fb5289
yafrees <yafree18@gmail.com>
2016-05-31 19:33:36 +0800

    新增NEXT按钮，设置按钮的监听&新增地理知识问答题目&抽取公共方法（Page38）

--- app/src/main/java/com/example/yf/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/yf/geoquiz/QuizActivity.java
@@ -4,17 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+
+    private TextView mQuestionTextView;
+
+    private Question [] mQuestionBank = new Question[]{
+            new Question(R.string.question_oceans , true),
+            new Question(R.string.question_mideast , true),
+            new Question(R.string.question_africa, true),
+            new Question(R.string.question_americas , true),
+            new Question(R.string.question_aisa , true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+//        int question = mQuestionBank[mCurrentIndex].getTextResID();x
+//        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +49,20 @@ public class QuizActivity extends AppCompatActivity {
                 Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    //对公共方法进行抽取，以方便共用。
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getTextResID();
+        mQuestionTextView.setText(question);
     }
 }
